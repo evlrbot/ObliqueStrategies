e1fa815c34acf6d43be66459225df72101cf7faa
Ryan & Sam <ryan@stad.io>
2017-01-04 10:34:16 -0800

    refactoring, removing, and cleaning up

--- startmeup.sh
+++ startmeup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo ROOT_URL=http://dev2.stad.io nohup meteor -p 8080 &
-#sudo ROOT_URL=http://dev2.stad.io meteor -p 80 
+#sudo ROOT_URL=http://src.dev.stad.io nohup meteor -p 8080 &
+sudo ROOT_URL=http://src.dev.stad.io meteor --unsafe-perm -p 80 
