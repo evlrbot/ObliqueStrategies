9cdd0497e03f3839de1ef06e10e2997b0558e8e2
Ryan & Sam <ryan@stad.io>
2018-04-19 03:22:28 -0700

    moved card phrase objects into their own files for clarity

--- main.js
+++ main.js
@@ -1,4 +1,43 @@
 if (Meteor.isClient) {
+    var programming_strategies = [
+"Solve the easiest possible problem in the dumbest possible way.",
+"Write a test for it.",
+"Is there a better name for this thing?",
+"Can we move work between query time (when we need the answer) and ingest time (when we see the data that eventually informs the answer)?",
+"Is it easier in a relational data store? A KV Store? A column store? A document store? A graph store?",
+"Can performance be improved by batching many small updates?",
+"Can clarity be improved by transforming a single update to more smaller updates?",
+"Can we more profitably apply a functional or declarative or imperative paradigm to the existing design?",
+"Can we profitably apply a change from synchronous to asynchronous, or vice versa?",
+"Can we profitably apply an inversion of control, moving logic between many individual call sites, a static central definition, and a reflectively defined description of the work to be done?",
+"Is it faster with highly mutable or easier with completely immutable data structures?",
+"Is it easier on the client side or the server side?",
+"List the transitive closure of fields in a data model. Regroup them to make the most sense for your application. Do you have the same data model?",
+"Is it better to estimate it quickly or compute it slowly?",
+"What semantics do you need? Should it be ordered? Transactional? Blocking?",
+"Can you do it with a regex? Do you need to bite the bullet and make a real parser? Can you avoid parsing by using a standardized format? (A few to get you started: s-expressions/XML/protobuf/JSON/yaml/msgpack/capn/avro/edn.)",
+"What is the schema for this data? Is the schema holding you back?",
+"Draw a state diagram according to the spec.",
+"Draw a state diagram according to the data.",
+"Draw a data flow (dX/dydX/dy)",
+"Draw a timeline (dX/dtdX/dt)",
+"How would you do it in Haskell? C? Javascript?",
+"Instead of doing something, emit an object.",
+"Instead of emitting an object, do something.",
+"Store all of it.",
+"Truncate the old stuff.",
+"Write the API you wish existed.",
+"Make an ugly version where all the things work.",
+"Make a gorgeous version that doesn't do anything.",
+"Can you codegen the boilerplate?",
+"Enumerate all the cases.",
+"What happens if you do it all offline / precompute everything? What happens if you recompute every time? Can you cache it?",
+"Can you build an audit log?",
+"Think like a tree: ignore the book-keeping details and find the cleanest representation.",
+"Think like a stack: zoom in to the book-keeping details and ignore the structure.",
+"Replace your implementation with an implementation that computes how much work the real implementation does for that problem.",
+"What is the steady state?"
+    ]
     var strategies = [
 	"(Organic) machinery ",
 	"A line has two sides ",
@@ -203,7 +242,7 @@ if (Meteor.isClient) {
     }
     
     // pick the first card
-    Session.set("card", pick(strategies));
+    Session.set("card", pick(programming_strategies));
     
     // display the current card
     Template.card_text.card_text = function(){
@@ -212,7 +251,7 @@ if (Meteor.isClient) {
     
     // handle "card" click events
     Template.card.events = {
-	'click .flip-container': function() {
+	'tap .flip-container, click .flip-container': function() {
 	    $(".flip-container").toggleClass("hover");
 	    if($(".flip-container").hasClass("hover")) {
 		Session.set("card", pick(strategies));
